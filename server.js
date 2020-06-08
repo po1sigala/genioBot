40c828c40c64096de38e1e7b2b17621a0cea4a66
Your Name <alexsigala87@gmail.com>
2020-06-08 13:33:13 -0700

    set up home route and edited post route to resoind with a json

--- server.js
+++ server.js
@@ -2,16 +2,20 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 const app = express();
+const cards = [];
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.get("/", (req, res) => {
-    console.log("got it");
-    console.log(req);
+    console.log(__dirname);
+    res.sendFile(path.join(__dirname, "home.html"));
 });
 app.post("/api/newcard", (req, res) => {
     console.log("posted");
-    console.log(req);
+
+    const newREs = req.body;
+    cards.push(newREs);
+    res.json();
 });
 app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}`);
